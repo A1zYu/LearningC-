3d4bc5d18dc28f0ea319b045846750f78cc6a6bf
A1zYu <2yusupchik1@mail.ru>
2024-07-12 01:32:25 +0300

    blacJack

---
+++ BlackJack/Program.cs
@@ -0,0 +1,33 @@
+﻿var isContinuePlayer = true;
+var isContinueComputer = true;
+var random = new Random();
+var currentComputer = 0;
+do
+{
+    Console.WriteLine("Будешь тянуть карту? (y)-тянешь карту (n)-пассуешь");
+    var answer = Console.ReadLine();
+    if (answer == "y")
+    {
+        Console.WriteLine("Вы тянете карту");
+    }
+    if (answer == "n")
+    {
+        Console.WriteLine("Вы пасуете");
+        isContinuePlayer = false;
+    }
+    
+    if (isContinueComputer)
+    {
+        currentComputer = random.Next(0, 2);
+    }
+    if (currentComputer ==0)
+    {
+        Console.WriteLine("Компьютер тянет карту");
+    }
+    if (currentComputer==1)
+    {
+        Console.WriteLine("Компьютер пасуете");
+        isContinueComputer = false;
+    }
+}
+while (isContinuePlayer || isContinueComputer);
